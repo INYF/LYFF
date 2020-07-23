1980bfe61eb23edec60294e29c29b5fcf3fa7aca
VA <vishalanuragi123@gmail.com>
2020-07-23 15:52:03 +0530

    23/07/20

---
+++ app/src/main/java/com/example/lyff/Donor/DetailsActivity_Donor.java
@@ -0,0 +1,16 @@
+package com.example.lyff.Donor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lyff.R;
+
+public class DetailsActivity_Donor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_details__donor );
+    }
+}
